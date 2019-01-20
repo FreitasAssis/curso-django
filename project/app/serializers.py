42dacd07a16ccafdb7a5089040119cbd5c7ea3d4
Luiz Antonio <luizvpc@gmail.com>
2019-01-20 11:54:56 -0300

    [ADD] - forms [ADD] - rest_framework

---
+++ project/app/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Post
+from django.contrib.auth import authenticate
+
+
+class PostModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('author', 'title', 'text')
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    username = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        self.username = attrs.get('username')
+        self.password = attrs.get('password')
+
+        user = authenticate(
+            request=self.context.get('request'),
+            username=self.username,
+            password=self.password
+        )
+        if not user:
+            msg = _('Unable to authenticat with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from rest_framework import viewsets
+from .serializers import PostModelSerializer,AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 def post_list(request):
@@ -11,3 +16,43 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'app/post_detail.html', {'post': post})
+
+
+def post_new(request):
+     if request.method == "POST":
+         form = PostForm(request.POST)
+         if form.is_valid():
+             post = form.save(commit=False)
+             post.author = request.user
+             post.published_date = timezone.now()
+             post.save()
+             return redirect('post_detail', pk=post.pk)
+     else:
+         form = PostForm()
+     return render(request, 'app/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+     post = get_object_or_404(Post, pk=pk)
+     if request.method == "POST":
+         form = PostForm(request.POST, instance=post)
+         if form.is_valid():
+             post = form.save(commit=False)
+             post.author = request.user
+             post.published_date = timezone.now()
+             post.save()
+             return redirect('post_detail', pk=post.pk)
+     else:
+         form = PostForm(instance=post)
+     return render(request, 'app/post_edit.html', {'form': form})
+
+
+class PostModelViewSet(viewsets.ModelViewSet):
+    serializer_class = PostModelSerializer
+    queryset = Post.objects.all().order_by('-title')
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create  a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
